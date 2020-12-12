28ab965b590c293c9ad4c18fd940e85a5d9c1cf6
Loan Alouache <alouache.loan@gmail.com>
2020-12-12 19:34:17 +0100

    Added game endpoint

--- src/game/game.py
+++ src/game/game.py
@@ -1,4 +1,4 @@
 from game.map.map_controller import MapController
 
-map_controller = MapController()
+map_controller: MapController = MapController()
 map_controller.init("../assets/maps/map00.json")
